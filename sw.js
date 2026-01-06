6689a3b834daa29bfed3ae9956ed985203606929
LiorShur <96306484+LiorShur@users.noreply.github.com>
2026-01-06 14:28:23 +0200

    Update CACHE_VERSION to v2.9.2-pwa-install-debug

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * - Images: Cache First
  */
 
-const CACHE_VERSION = 'v2.9.1-pwa-install-sync';
+const CACHE_VERSION = 'v2.9.2-pwa-install-debug';
 const APP_CACHE = `access-nature-app-${CACHE_VERSION}`;
 const DATA_CACHE = `access-nature-data-${CACHE_VERSION}`;
 const MAP_CACHE = `access-nature-maps-${CACHE_VERSION}`;
