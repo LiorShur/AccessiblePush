1f08c476cd3280d36d469f41e8efa8d5911f1c8b
LiorShur <96306484+LiorShur@users.noreply.github.com>
2026-01-16 17:20:57 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * - Images: Cache First
  */
 
-const CACHE_VERSION = 'v2.9.65-navmenu-position-fix';
+const CACHE_VERSION = 'v2.9.66-mobility-hide-fix';
 const APP_CACHE = `access-nature-app-${CACHE_VERSION}`;
 const DATA_CACHE = `access-nature-data-${CACHE_VERSION}`;
 const MAP_CACHE = `access-nature-maps-${CACHE_VERSION}`;
